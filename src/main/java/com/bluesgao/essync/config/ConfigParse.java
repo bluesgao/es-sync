698c237d2f587e77bf8b6ce4d35b811ec97003e9
gaoxin11 <gaoxin11@jd.com>
2019-09-23 20:50:49 +0800

    config

--- src/main/java/com/bluesgao/essync/config/ConfigParse.java
+++ src/main/java/com/bluesgao/essync/config/ConfigParse.java
@@ -1,8 +1,21 @@
 package com.bluesgao.essync.config;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.List;
+
 /**
  * config 解析
  * 1,parse 将json字符串解析为syncConfig对象
  */
 public class ConfigParse {
+    public static List<SyncConfig> parse(String cfgStr) {
+        List<SyncConfig> syncConfigs = null;
+        try {
+            syncConfigs = JSON.parseArray(cfgStr, SyncConfig.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return syncConfigs;
+    }
 }
